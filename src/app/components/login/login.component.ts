7697b5ffeb063561ee92d6a79b8ff816fed0d8da
AbdMaker-dev <dioufbadaraalioune7@gmail.com>
2020-12-09 11:25:41 +0000

    registration

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  userForm: any = {};
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+  }
+
+  login(user: any) {
+    this.http.post("http://localhost:3000/login", user).subscribe(data => {
+      console.log(data);
+      return data;
+    },
+      err => {
+
+      });
+  }
+
+  onSubmit() {
+
+    this.login({ "email": this.userForm.email, "password": this.userForm.password })
   }
 
 }
